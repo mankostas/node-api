dd7c4f2a5372f02537fcca008f78d6045b22aa27
mankostas <anastasiou.ks@gmail.com>
2023-06-26 13:38:28 +0300

    Updated Entry point of the app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { errorMiddleware } from './middleware/errorHandling';
-import { articleRoutes } from './routes/api/V1/articleRoutes';
+import { articleRoutes } from './routes/api/V1/eventRoutes';
 import { db } from "./includes/db"
 // import { Event } from "./entities/Event"
 
@@ -30,29 +30,6 @@ db.initialize();
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
-// // Test Insert in db
-// db.initialize().then(async () => {
-
-//     console.log("Inserting a new user into the database...")
-//     const article = new Event()
-//     article.id = 1
-//     article.title = "Test Event"
-//     article.description = "Test article description"
-//     article.eventDate = new Date('2023-08-15 05:00:00');
-//     article.location = "Kreontos 27"
-//     await db.manager.save(article)
-//     console.log("Saved a new user with id: " + article.id)
-
-//     console.log("Loading users from the database...")
-//     const users = await db.manager.find(Event)
-//     console.log("Loaded users: ", users)
-
-//     console.log("Here you can setup and run express / fastify / any other framework.")
-
-// }).catch(error => console.log(error))
-
 
  export default app;
 
