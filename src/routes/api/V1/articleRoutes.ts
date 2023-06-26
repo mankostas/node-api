8dff003342c26e72a02bc264e7cc30dd6352e147
mankostas <anastasiou.ks@gmail.com>
2023-06-26 12:17:18 +0300

    Changed some typos regarding the Event Contoller

--- src/routes/api/V1/articleRoutes.ts
+++ src/routes/api/V1/articleRoutes.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import { articleController } from '../../../controllers/api/V1/ArticleController';
+import { eventController } from '../../../controllers/api/V1/EventController';
 
 export const articleRoutes = express.Router();
 
-articleRoutes.get('/', articleController.getAllEvents);
-articleRoutes.post('/', articleController.createEvent); 
-articleRoutes.get('/:id', articleController.getEvent);
-articleRoutes.put('/:id', articleController.updateEvent);
+articleRoutes.get('/', eventController.getAllEvents);
+articleRoutes.post('/', eventController.createEvent); 
+articleRoutes.get('/:id', eventController.getEvent);
+articleRoutes.put('/:id', eventController.updateEvent);
